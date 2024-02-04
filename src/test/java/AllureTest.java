beba0a9983286503125208ca0bd74b910fa7750e
lutencjusz <lutencjusz@gmail.com>
2024-02-04 16:50:56 +0100

    dodanie AllureTest

--- src/test/java/AllureTest.java
+++ src/test/java/AllureTest.java
@@ -2,7 +2,9 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.model.*;
 import org.junit.Test;
 
+import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 import java.util.UUID;
 
 public class AllureTest {
@@ -31,11 +33,11 @@ public class AllureTest {
 
         Parameter parameter1 = new Parameter()
                 .setName("testResult1.Parametr 1")
-                .setValue("testResult1.Wartość 1");
+                .setValue("testResult1.parameter1.setValue 1");
 
         Parameter parameter2 = new Parameter()
                 .setName("testResult1.Parametr 2")
-                .setValue("testResult1.Wartość 2");
+                .setValue("testResult1.parameter1.setValue 2");
 
         StatusDetails statusDetailsTestResult = new StatusDetails()
                 .setMessage("Wystąpił błąd podczas wykonywania testu 1 - testResult1.statusDetails.setMessage")
@@ -54,6 +56,23 @@ public class AllureTest {
                 .setSource(attachmentUuid + ".txt");
 
         // Tworzenie wyników testu
+        String testResult2Uuid = UUID.randomUUID().toString();
+        TestResult testResult2 = new TestResult()
+                .setUuid(testResult2Uuid)
+                .setName("Test Result 2 - określony w testResult2.setName")
+                .setFullName("Test Result 2 - określony w testResult2.setFullName")
+                .setDescriptionHtml("<i style='color:green;'>tekst 1 określony testResult2.setDescriptionHtml</i>")
+                .setHistoryId("testResult2.setHistoryId")
+                .setStage(Stage.FINISHED)
+                .setStart(System.currentTimeMillis())
+                .setStop(System.currentTimeMillis() + 1000)
+                .setTestCaseId("identyfikator - testResult2.setTestCaseId")
+                .setTestCaseName("Przypadek użycia - testResult2.setTestCaseName")
+                .setStatus(Status.PASSED);
+
+        testResult2.getLabels().add(label1);
+        testResult2.getLabels().add(label2);
+
         String testResult1Uuid = UUID.randomUUID().toString();
         TestResult testResult1 = new TestResult()
                 .setUuid(testResult1Uuid)
@@ -81,6 +100,9 @@ public class AllureTest {
         // Dodawanie kroków do wyniku testu
         String stepUuid1 = UUID.randomUUID().toString();
         StepResult stepResult1 = new StepResult().setName(testResult1.getTestCaseName() + ": Step 1").setStatus(Status.PASSED);
+
+        String stepUuid2 = UUID.randomUUID().toString();
+        StepResult stepResult2 = new StepResult().setName(testResult2.getTestCaseName() + ": Step 1").setStatus(Status.PASSED);
 
         // Dodawanie podkroków do StepResult1
         String substepUuid1 = UUID.randomUUID().toString();
@@ -116,15 +138,16 @@ public class AllureTest {
 
         // Dodawanie parametrów do SubsubstepResult2
         Parameter parameterSubsubstepResult1 = new Parameter()
-                .setName("Parametr 1")
-                .setValue("Wartość 1");
+                .setName("SubsubstepResult2.Parametr 1")
+                .setValue("Wartość 1 - SubsubstepResult2.Parametr.setValue");
 
         Parameter parameterSubsubstepResult2 = new Parameter()
-                .setName("Parametr 2")
-                .setValue("Wartość 2");
+                .setName("SubsubstepResult2.Parametr 2")
+                .setValue("Wartość 2 - SubsubstepResult2.Parametr.setValue");
 
         subsubstepResult2.getParameters().add(parameterSubsubstepResult1);
         subsubstepResult2.getParameters().add(parameterSubsubstepResult2);
+        subsubstepResult2.setDescriptionHtml("<i style='color:red;'>tekst 2 określony subsubstepResult2.setDescriptionHtml</i>");
 
         Attachment attachment = new Attachment()
                 .setName("Załącznik subsubstepResult2")
@@ -152,17 +175,71 @@ public class AllureTest {
 
         testResult1.getSteps().add(stepResult1);
 
+        Allure.getLifecycle().startStep(testResult2Uuid, stepUuid2, stepResult2);
+        Allure.getLifecycle().stopStep(stepUuid2);
+
+        testResult2.getSteps().add(stepResult2);
+
+        // Wygenerowanie pliku environment.properties
+        Properties properties = new Properties();
+        properties.setProperty("os_name", System.getProperty("os.name"));
+        properties.setProperty("os_version", System.getProperty("os.version"));
+        properties.setProperty("java_version", System.getProperty("java.version"));
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("allure-results/environment.properties");
+            properties.store(fileOutputStream, "Allure environment properties");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Tworzenie fixture'a
+        String fixtureUuid = UUID.randomUUID().toString();
+        FixtureResult fixtureResult = new FixtureResult()
+                .setName("Fixture utworzony w FixtureResult.setName")
+                .setStatus(Status.PASSED);
+
         // Zapisywanie wyników do raportu Allure
         Allure.getLifecycle().startTestContainer(root);
         Allure.getLifecycle().startTestContainer(child);
+        Allure.getLifecycle().startPrepareFixture(testResult2.getUuid(), fixtureUuid, fixtureResult);
+        Allure.getLifecycle().stopFixture(fixtureUuid);
         Allure.getLifecycle().scheduleTestCase(testResult1);
         Allure.getLifecycle().startTestCase(testResult1.getUuid());
+
+        Allure.epic("Epik ustawiony w Allure.epic");
+        Allure.feature("Feature ustawiona w Allure.feature");
+        Allure.story("Story ustawiona w Allure.story");
+        Allure.suite("Suite ustawione w Allure.suite");
+        Allure.label("label1", "labelValue1 - ustawione w Allure.label");
+        Allure.parameter("Parametr 1", "Wartość 1 - ustawione w Allure.parameter");
+        Allure.parameter("Parametr 2", "Wartość 2 - ustawione w Allure.parameter");
+        Allure.issue("issue - ustawione w Allure.issue", "https://www.google.pl/");
+        Allure.tms("tms - ustawione w Allure.tms", "https://www.google.pl/");
+        Allure.link("link - ustawione w Allure.link", "https://www.google.pl/");
+
         Allure.getLifecycle().addAttachment(attachment.getName(), "text/plain", ".txt", attachmentBytes);
         Allure.getLifecycle().writeTestCase(testResult1.getUuid());
+        Allure.getLifecycle().scheduleTestCase(testResult2);
+        Allure.getLifecycle().startTestCase(testResult2.getUuid());
+
+        Allure.epic("Epik ustawiony w Allure.epic");
+        Allure.feature("Feature ustawiona w Allure.feature");
+        Allure.story("Story ustawiona w Allure.story");
+        Allure.suite("Suite ustawione w Allure.suite");
+        Allure.label("label1", "labelValue1 - ustawione w Allure.label");
+        Allure.parameter("Parametr 1", "Wartość 1 - ustawione w Allure.parameter");
+        Allure.parameter("Parametr 2", "Wartość 2 - ustawione w Allure.parameter");
+        Allure.issue("issue - ustawione w Allure.issue", "https://www.google.pl/");
+        Allure.tms("tms - ustawione w Allure.tms", "https://www.google.pl/");
+        Allure.link("link - ustawione w Allure.link", "https://www.google.pl/");
+
+        Allure.getLifecycle().writeTestCase(testResult2.getUuid());
         Allure.getLifecycle().stopTestContainer(child.getUuid());
         Allure.getLifecycle().writeTestContainer(child.getUuid());
         Allure.getLifecycle().stopTestContainer(root.getUuid());
         Allure.getLifecycle().writeTestContainer(root.getUuid());
+
+
     }
 
 
