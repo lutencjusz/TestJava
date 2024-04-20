5d22475b2ee8a3fceb7f4634b109a5b8956cd72b
lutencjusz <lutencjusz@gmail.com>
2024-04-20 13:07:30 +0200

    dodanie ChatGPT

--- src/main/java/ChatGPT.java
+++ src/main/java/ChatGPT.java
@@ -10,10 +10,28 @@ public class ChatGPT {
             .load();
 
     private static final String CHAT_GPT_KEY = dotenv.get("CHAT_GPT_KEY");
-    private static final String ENDPOINT = "https://api.openai.com/v1/chat/completions";
+    private static final String CHAT_GPT_ENDPOINT = "https://api.openai.com/v1/chat/completions";
+    private static final String JAVELIN_ENDPOINT = "http://localhost:7000";
 
-    public static void main(String[] args) {
-        RestAssured.baseURI = ENDPOINT;
+    /**
+     * Demo Javelin REST service, do uruchomienia wymagane jest uruchomienie projektu JavelinRestServiceTest
+     * (C:\Data\Java\TestJava\src\main\java\JavelinRestServiceTest.java)
+     */
+    public static void demoJavelin() {
+        RestAssured.baseURI = JAVELIN_ENDPOINT;
+        RequestSpecification request = RestAssured.given();
+
+        Response response = request
+                .header("Content-Type", "application/json")
+                .get("/hello");
+
+        System.out.println("Response Status Code: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
+        System.out.println("message: " + response.jsonPath().get("message"));
+    }
+
+    public static void chatGPT() {
+        RestAssured.baseURI = CHAT_GPT_ENDPOINT;
         RequestSpecification request = RestAssured.given();
 
         String requestBody = "{"
@@ -29,5 +47,10 @@ public class ChatGPT {
 
         System.out.println("Response Status Code: " + response.getStatusCode());
         System.out.println("Response Body: " + response.getBody().asString());
+    }
+
+    public static void main(String[] args) {
+        chatGPT();
+//        demoJavelin();
     }
 }
