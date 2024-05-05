3cda5d71520b9b431368bc6d8d17ed644b28c522
lutencjusz <lutencjusz@gmail.com>
2024-05-05 20:30:06 +0200

    Parametry produce'rów

--- src/main/java/Kafka/SimpleProducer.java
+++ src/main/java/Kafka/SimpleProducer.java
@@ -27,20 +27,26 @@ public class SimpleProducer {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class);
         props.put(ProducerConfig.CLIENT_ID_CONFIG, "SimpleProducer");
+//        props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, SimplePartitioner.class);
+        props.put(ProducerConfig.ACKS_CONFIG, "0");
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 5);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5);
+        props.put(ProducerConfig.RETRIES_CONFIG, 2);
 
         Producer<String, Product> producer = new KafkaProducer<>(props);
 
         try {
             for (int i = 0; i < 2; i++) {
                 String message = "Message " + i;
-                producer.send(new ProducerRecord<>(topicName, null, new Product(message,1))).get();
+                producer.send(new ProducerRecord<>(topicName, null, new Product(message, 1))).get();
                 System.out.println("Sent:" + message);
             }
 
-            producer.send(new ProducerRecord<>(topicName, "testowy-klucz-2", new Product("Produkt testowy 1", 1.1))).get();
-            producer.send(new ProducerRecord<>(topicName, "testowy-klucz-3", new Product("Produkt testowy 2",1.2)), (recordMetadata, e) -> {
+            producer.send(new ProducerRecord<>(topicName, 3, "testowy-klucz-2", new Product("Produkt testowy 1 na partycji 3", 1.1))).get();
+            producer.send(new ProducerRecord<>(topicName, "testowy-klucz-3", new Product("Produkt testowy 2", 1.2)), (recordMetadata, e) -> {
                 if (e != null) {
-                    e.printStackTrace();
+                    System.out.println("Błąd wysyłania wiadomości: " + e.getMessage());
                 } else {
                     System.out.printf("Wysłano komunikat:(topic: %s, partition: %s, offset: %s) ", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
                 }
